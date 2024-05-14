36834457f70ad02e0154fb873b1d1ad469121ce9
Iftakharul Islam <115061454+iftakharul-xs@users.noreply.github.com>
2024-05-14 11:25:09 +0600

    user table modified

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('role')->default(\App\Constants\Role::USER);
             $table->rememberToken();
             $table->timestamps();
         });
